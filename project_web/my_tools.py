f01fffa9aa46a6d9a6b06dd312cc51ca4a6d128f
victorz <459401748@qq.com>
2018-08-16 02:22:26 +0800

    add date_time_format function

--- project_web/my_tools.py
+++ project_web/my_tools.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import datetime
+
 def get_error_message(error_code, error_msg, data):
     res = {
         "error_code" : error_code,
@@ -9,3 +11,7 @@ def get_error_message(error_code, error_msg, data):
     }
     print(str(error_code) + ":" + error_msg + "    " + str(data))
     return res
+
+def date_time_format(date_time):
+    time = datetime.datetime.strftime(date_time,'%Y-%m-%d %H:%M:%S')
+    return time
