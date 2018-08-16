df09e510d99f0929b59f06476ee579f04350f858
victorz <459401748@qq.com>
2018-08-17 01:03:53 +0800

    add fixed temperature route

--- project_web/app.py
+++ project_web/app.py
@@ -9,7 +9,7 @@ import threading, datetime
 
 app = Flask(__name__)
 my_temperature_changer = None
-my_aircon = aircon()
+aircon_bedroom = aircon("bedroom")
 
 
 '''
@@ -31,18 +31,24 @@ t like this
 def home():
     if my_temperature_changer:
         t = my_temperature_changer.get_sensor_data()
-    aircon_data = my_aircon.get_aircon_data()
+    aircon_data = aircon_bedroom.get_aircon_data()
     t.update(aircon_data)
     return render_template("index.html", temperature=t)
 
 @app.route('/open', methods = ['POST'])
 def open():
     data = request.get_data().decode('utf-8')
-    res = my_aircon.ac_operate(data)
+    res = aircon_bedroom.ac_operate(data)
     return res.get("error_msg")
 
+@app.route('/fixed_temperature', methods = ['POST'])
+def fixed_temperture():
+    return aircon_bedroom.set_fixed_temperature()
+
 if __name__ == '__main__':
+    aircon = [aircon_bedroom]
     my_temperature_changer = my_temperature_looper()
+    my_temperature_changer.set_aircon(aircon)
     my_temperature_changer.start_thread()
     # If want request from other hosts,shoud change param like this.
     app.run(host='0.0.0.0')
