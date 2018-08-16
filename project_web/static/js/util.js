d8612e63632c85df4ddb12f9c1ca81a510147e7f
victorz <459401748@qq.com>
2018-08-17 01:02:17 +0800

    fix can not call onreadystatechange

--- project_web/static/js/util.js
+++ project_web/static/js/util.js
@@ -1,17 +1,19 @@
-var ajax = function(request_message, success_function) {
-    msg = request_message;
-    success_function == success_function || function(){
-        return 0;
+var ajax = function(request_message, success_function, request_url) {
+    var msg = request_message;
+    var req_url = request_url || '/open'
+    success_function = success_function || function(){
+        console.log("success")
     }
     var a = new XMLHttpRequest();
+    a.open('post', req_url);
+    a.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    a.send(msg);
     a.onreadystatechange = function() {
-        if(a.readyStat == 4 && a.status == 200) {
+        if(a.readyState == 4 && a.status == 200) {
             success_function();
             var message = a.responseText;
             console.log(message);
+            window.location.reload()
         }
     }
-    a.open('post', '/open');
-    a.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    a.send(msg);
 }
