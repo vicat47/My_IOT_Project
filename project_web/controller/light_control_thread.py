bee2df6f0ee5fb6fe7f5964e5161a6b992c17b3e
zsb514 <459401748@qq.com>
2018-10-16 17:45:23 +0800

    debug the power open/close problem

--- project_web/controller/light_control_thread.py
+++ project_web/controller/light_control_thread.py
@@ -1,6 +1,9 @@
 import time,datetime
 import entity.xiaomi_light,dao.human_detect
 from threading import Thread
+from util.my_tools import debug
+
+DEBUG = True
 
 class human_sensor_control_light:
     def __init__(self, sensor, light):
@@ -15,16 +18,17 @@ class human_sensor_control_light:
         while self.RUNNING:
             now = datetime.datetime.now()
             if self.human_detect_sensor.has_person:
-                if (now - self.human_detect_sensor.last_none_person).seconds > 120:
-                    if self.xiaomi_light.status.get("power") is "off":
-                        if (now - self.xiaomi_light.status.get("last_open")).seconds > 120:
-                            self.xiaomi_light.send("power_on")
+                    debug(DEBUG, "现在有人!")
+                    if (now - self.xiaomi_light.status.get("last_open")).seconds > 120:
+                        debug(DEBUG, "开灯")
+                        self.xiaomi_light.send("power_on")
                 self.human_detect_sensor.last_has_person = now
             else:
                 if (now - self.human_detect_sensor.last_has_person).seconds > 120:
-                    if self.xiaomi_light.status.get("power") is "on":
-                        if (now - self.xiaomi_light.status.get("last_close")).seconds > 120:
-                            self.xiaomi_light.send("power_off")
+                    debug(DEBUG, "连续两分钟没人")
+                    if (now - self.xiaomi_light.status.get("last_close")).seconds > 120:
+                        self.xiaomi_light.send("power_off")
+                        time.sleep(120)
                 self.human_detect_sensor.last_none_person = now
             time.sleep(6)
             
